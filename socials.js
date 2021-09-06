594fa7e13d2ac9ef8078cc5a36182fcb42bd4357
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2021-09-06 09:49:13 -0700

    Update socials.js

--- socials.js
+++ socials.js
@@ -5,6 +5,9 @@ fetch('/socials.json').then(function (res){
   document.getElementById("Git").href = 'https://github.com/' + json.GitHub
   document.getElementById("Insta").href = 'https://instagram.com/' + json.Instagram
   document.getElementById("Instalt").href = 'https://instagram.com/' + json.Instagramalt
+  document.getElementById("Instalt1").href = 'https://instagram.com/' + json.Instagramalt1
+  document.getElementById("Instalt2").href = 'https://instagram.com/' + json.Instagramalt2
+  document.getElementById("Instalt3").href = 'https://instagram.com/' + json.Instagramalt3
   document.getElementById("Dis").href = 'https://discord.gg/' + json.Discord
   document.getElementById("Mail").href = 'mailto:' + json.Email
   document.getElementById("Twit").href = 'https://twitter.com/' + json.Twitter
