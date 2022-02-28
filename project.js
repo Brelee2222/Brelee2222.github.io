b57e36b011982e4345acf2028a28b3218c2e39db
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2022-02-28 14:32:27 -0800

    Delete project.js

--- project.js
+++
@@ -1,5 +0,0 @@
-fetch('https://raw.githubusercontent.com/Brelee2222/My-current-projects/main/projects.txt').then(function (res) {
-    return res.text()
-  }).then(function (text) {
-    document.getElementById("disp").innerHTML = text
-  });
